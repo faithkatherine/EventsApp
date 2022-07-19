be8e127628da6a60bfd377fae20eeaaa2a13ab59
faithkatherine <faithcathy12@gmail.com>
2022-07-19 15:49:11 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.components';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+@NgModule({
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input , Output} from "@angular/core";
+import { EventEmitter } from "protractor";
+
+@Component({
+    selector: 'event-thumbnail',
+    template:  `
+    <div>
+        <h1>Upcoming Angular Events</h1>
+        <hr>
+        <div class="well hoverwell thumbnail">
+            <h2>{{event.name}}</h2>
+            <div>Date: {{event.date}}</div>
+            <div>Time: {{event.time}}</div>
+            <div>Price: \${{event.price}}</div>
+            <div>
+                <span>Location:{{event.location.address}}</span>
+                <span>&nbsp;</span>
+                <span> {{event.location.city}}, {{event.location.country}}</span>
+            </div>
+            <button class="btn btn-primary" (click)= "handleClickMe()" > Click Me </button>
+        </div>
+        
+    </div>
+    `
+})
+
+export class EventThumbnailComponent{
+    @Input() event: any
+    @Output() eventClick = new EventEmitter()
+    handleClickMe(){
+        this.eventClick.emit('foo')
+    }
+
+}
